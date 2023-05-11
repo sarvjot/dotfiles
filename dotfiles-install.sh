c1b1081e06e0b9a34a3ac925a12f2757686d3db4
sarvjot <sarvjotsingh422002@gmail.com>
2023-05-11 11:16:57 +0530

    remove packer_compiled and add environment switching functions in script

--- dotfiles-install.sh
+++ dotfiles-install.sh
@@ -2,3 +2,15 @@ ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.p10k.zsh ~/.p10k.zsh
 ln -s ~/.dotfiles/.fzf.zsh ~/.fzf.zsh
 ln -s ~/.dotfiles/.config ~/.config
+
+use_development() {
+	rm -rf ~/.local/share/nvim
+	rm -rf ~/.dotfiles/.config/nvim
+	ln -s nvim-dev nvim 
+}
+
+use_production() {
+	rm -rf ~/.local/share/nvim
+	rm -rf ~/.dotfiles/.config/nvim
+	ln -s nvim-prod nvim 
+}
