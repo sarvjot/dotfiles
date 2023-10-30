e3a8da22a74847ad7639d620d585e5c2e37de2c2
sarvjot <sarvjotsingh422002@gmail.com>
2023-10-30 17:02:17 +0530

    nothing much

--- .config/nvim-dev/lua/myconfig/gitsigns.lua
+++ .config/nvim-dev/lua/myconfig/gitsigns.lua
@@ -1,15 +1,15 @@
 local M = {}
 
 function M.setup()
-  require('gitsigns').setup {
-    signs = {
-      add = { text = '+' },
-      change = { text = '~' },
-      delete = { text = '_' },
-      topdelete = { text = '‾' },
-      changedelete = { text = '~' },
-    },
-  }
+	require('gitsigns').setup {
+		signs = {
+			add = { text = '+' },
+			change = { text = '~' },
+			delete = { text = '_' },
+			topdelete = { text = '‾' },
+			changedelete = { text = '~' },
+		},
+	}
 end
 
 return M
